a6d7495281428d318a7c955ec5cacd117ad40c7e
John Doe <john@doe.org>
2016-05-12 15:07:57 +0800

    2016-5-12

---
+++ portal/protected/views/background/index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <title>官网首页</title>
+    <link rel="stylesheet" type="text/css" href="style/base.css" />
+    <link rel="stylesheet" type="text/css" href="style/layout.css" />
+</head>
+
+<body style="background:#6c5256;">
+    <div class="wapper index_container">
+        <!--header start-->
+        <div class="header">
+            <h1 class="title">YOU ARE BEST</h1>
+            <span class="sub_title">Where there's a will, there's a way.</span>
+        </div>
+        <!--header end-->
+        <!--nav start-->
+        <div class="nav_box">
+            <ul class="nav_list clearfix">
+                <li><a class="active" href="javascript:;">HOME</a>
+                </li>
+                <li><a href="javascript:;">UPLOAD</a>
+                </li>
+                <li><a href="javascript:;">TRENDS</a>
+                </li>
+                <li><a href="javascript:;">ABOUT US</a>
+                </li>
+            </ul>
+        </div>
+        <!--nav end-->
+        <!--con start-->
+        <div class="index_con_box">
+            <div class="con">
+                <div class="top clearfix">
+                    <div class="left clearfix">
+                        <div class="select_box left">
+                            <div class="my_select clearfix">
+                                <span class="select_con">请选择</span>
+                                <span class="down"></span>
+                            </div>
+                            <select class="select_list" name="" id="">
+                                <option value="请选择">请选择</option>
+                                <option value="上传时间(由新到旧)">上传时间(由新到旧)</option>
+                                <option value="上传时间(由新到旧新到新到新到)">上传时间(由新到旧)</option>
+                                <option value="上传时间(由新到旧)">上传时间(由新到旧)</option>
+                            </select>
+                        </div>
+                        <div class="select_box left">
+                            <div class="my_select clearfix">
+                                <span class="select_con">请选择</span>
+                                <span class="down"></span>
+                            </div>
+                            <select class="select_list" name="" id="">
+                                <option value="请选择">请选择</option>
+                                <option value="上传时间(由新到旧)">上传时间(由新到旧)</option>
+                                <option value="上传时间(由新到旧新到新到新到)">上传时间(由新到旧)</option>
+                                <option value="上传时间(由新到旧)">上传时间(由新到旧)</option>
+                            </select>
+                        </div>
+                        <p class="left">共<span class="num">1</span>个视频</p>
+                    </div>
+                    <button class="right upload_new_btn">上传新视频</button>
+                </div>
+                <ul class="upload_list">
+                    <li class="clearfix">
+                        <div class="upload_con_box left clearfix">
+                            <div class="video_img left">
+                                <img src="images/video_img.jpg" alt="">
+                                <span>私密视频</span>
+                            </div>
+                            <div class="video_info left">
+                                <h3>口袋分销演示</h3>
+                                <div class="state_box clearfix">
+                                    <img class="left" src="images/up06.jpg" alt="">
+                                    <span class="left">刚刚上传</span>
+                                    <span class="from left">来自：爱奇艺网页</span>
+                                </div>
+                                <p class="tag">标签:<span>分销</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="edit_btn_box right clearfix">
+                            <span class="left state">转码中</span>
+                            <a class="edit_btn left" href="javascript:;">编辑</a>
+                        </div>
+                    </li>
+                    <li class="clearfix">
+                        <div class="upload_con_box left clearfix">
+                            <div class="video_img left">
+                                <img src="images/video_img.jpg" alt="">
+                                <span>私密视频</span>
+                            </div>
+                            <div class="video_info left">
+                                <h3>口袋分销演示</h3>
+                                <div class="state_box clearfix">
+                                    <img class="left" src="images/up06.jpg" alt="">
+                                    <span class="left">刚刚上传</span>
+                                    <span class="from left">来自：爱奇艺网页</span>
+                                </div>
+                                <p class="tag">标签:<span>分销</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="edit_btn_box right clearfix">
+                            <span class="left state">转码中</span>
+                            <a class="edit_btn left" href="javascript:;">编辑</a>
+                        </div>
+                    </li>
+                    <li class="clearfix">
+                        <div class="upload_con_box left clearfix">
+                            <div class="video_img left">
+                                <img src="images/video_img.jpg" alt="">
+                                <span>私密视频</span>
+                            </div>
+                            <div class="video_info left">
+                                <h3>口袋分销演示</h3>
+                                <div class="state_box clearfix">
+                                    <img class="left" src="images/up06.jpg" alt="">
+                                    <span class="left">刚刚上传</span>
+                                    <span class="from left">来自：爱奇艺网页</span>
+                                </div>
+                                <p class="tag">标签:<span>分销</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="edit_btn_box right clearfix">
+                            <span class="left state">转码中</span>
+                            <a class="edit_btn left" href="javascript:;">编辑</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!--con end-->
+        <div class="index_foot">
+            FOOT AREA
+        </div>
+    </div>
+    <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="js/select.js"></script>
+</body>
+
+</html>
