6987cec3233e5053c07b9fedfb90c9dce663dd9f
light_an <173549598@qq.com>
2016-05-13 11:15:44 +0800

    修改model文件名 增加上传Case、Product，新增供应商（包含新增至员工步骤）接口

--- portal/protected/controllers/BackgroundController.php
+++ portal/protected/controllers/BackgroundController.php
@@ -105,4 +105,88 @@ class BackgroundController extends InitController
         $this->render("index");
     }
 
+    public function actionCase_upload()
+    {
+        $_POST['CI_Name'] = 333;
+        $_POST['CI_Pic'] = 333;
+        $_POST['CI_Remarks'] = 333;
+
+        $data = new CaseInfo;  
+        $data ->CI_Name = $_POST['CI_Name']; 
+        $data ->CI_Place = "";
+        $data ->CI_Pic = $_POST['CI_Pic'];
+        // $data ->CI_Time = $_POST['CI_Time'];
+        $data ->CI_Sort = 1;
+        $data ->CI_Show = 1;
+        $data ->CI_Remarks = $_POST['CI_Remarks']; 
+        $data ->CI_Type = 1;
+        $data->save();
+
+        $CI_ID = $data->attributes['CI_ID'];
+        
+        $data = new CaseBind;
+        $data ->CI_Type = 1; 
+        $data ->TypeID = $_COOKIE['account_id'];
+        $data ->CI_ID = $CI_ID;
+        $data->save();
+
+        $data = new CaseResources;
+        $data ->CI_ID = $CI_ID;
+        $data ->CR_Sort = 1;
+        $data ->CR_Show = 1;
+        foreach ($_POST['resources'] as $key => $value) {
+            $data ->CR_Type = $vale['Cr_Type'];
+            $data ->CR_Name = $vale['Cr_Name'];
+            $data ->CR_Path = $vale['Cr_Path'];
+            $data ->CR_Remarks = $vale['Remarks'];
+            $data->save();
+        }
+    }
+
+    public function actionProduct_upload()
+    {
+        // $_POST['CI_Name'] = 333;
+        // $_POST['CI_Pic'] = 333;
+        // $_POST['CI_Remarks'] = 333;
+        $data = new SupplierProduct;
+        $data ->account_id = $_COOKIE['account_id'];
+        $data ->supplier_id = $_POST['supplier_id']; 
+        $data ->supplier_type_id = $_POST['supplier_type_id'];
+        $data ->standard_type = $_POST['standard_type'];
+        $data ->name = $_POST['name'];
+        $data ->category = $_POST['category'];
+        $data ->unit_price = $_POST['unit_price'];
+        $data ->unit_cost = $_POST['unit_cost'];
+        $data ->unit = $_POST['unit'];
+        $data ->service_charge_ratio = $_POST['service_charge_ratio'];
+        $data ->ref_pic_url = $_POST['ref_pic_url'];
+        $data ->decription = $_POST['decription'];
+        $data ->update_time = $_POST['update_time'];
+        $data->save();
+    }
+
+    public function actionSupplier_add()
+    {
+        $data = new Staff;
+        $data ->account_id = $_COOKIE['account_id'];
+        $data ->name = $_POST['name'];
+        $data ->telephone = $_POST['telephone'];
+        $data ->department_list = "[4]";
+        $data ->update_time = $_POST['update_time'];
+        $data ->save();
+        //查找新增的员工ID
+        $id = $data->attributes['id'];
+
+        //新增供应商
+        $data = new Supplier;
+        $data ->account_id = $_COOKIE['account_id'];
+        $data ->type_id = $_POST['type_id'];
+        $data ->staff_id = $id;
+        $data ->contract_url = $_POST['contract_url'];
+        $data ->update_time = $_POST['update_time'];
+        $data ->save();
+    }
+
+
+
 }
